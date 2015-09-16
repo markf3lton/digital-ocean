bdc939999d943abe7186fa547abb76dd6b3c1231
Mark Felton <markfelton@fastmail.fm>
2015-09-16 14:20:43 -0400

    My DO sites - redirecting

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Hello World - Redirect</title>
+    <META http-equiv="refresh" content="5;URL=http://p1.felton.link">
+  </head>
+  <body bgcolor="#ffffff">
+    <?php echo 'Hello World'; ?>
+    <center>You will be redirected to <a href="http://p1.felton.link">p1.felton.link</a> automatically in 5 seconds.
+    </center>
+  </body>
+</html>
